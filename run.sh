763b7cb8000e2e95e4c728eef36d62135d5a6d49
Mytheiw <128908320+Mytheiw@users.noreply.github.com>
2023-04-24 02:31:23 -1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMAGE_FILE="/root/run/image/pxfuel.jpg"
-AUDIO_FILE_OR_PLAYLIST_URL="https://soundcloud.com/xaviez-286294178/sets/blue-archive?si=0b569c62e8f74e6ba9bc112a0e9386ba&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+AUDIO_FILE_OR_PLAYLIST_URL="https://soundcloud.com/xaviez-286294178/sets/blue-archive?si=0b569c62e8f74e6ba9bc112a0e9386ba"
 YOUTUBE_RTMP_SERVER_URL="rtmp://x.rtmp.youtube.com/live2"
 YOUTUBE_STREAM_KEY="4mkp-0570-xf06-7gb5-41dx"
 
