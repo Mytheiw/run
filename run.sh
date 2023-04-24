82acdc2152b033251d51c02beac45442863abeb9
Mytheiw <128908320+Mytheiw@users.noreply.github.com>
2023-04-24 00:10:58 -1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ AUDIO_FILE_OR_PLAYLIST_URL="https://soundcloud.com/xaviez-286294178/sets/blue-ar
 YOUTUBE_RTMP_SERVER_URL="rtmp://x.rtmp.youtube.com/live2"
 YOUTUBE_STREAM_KEY="4mkp-0570-xf06-7gb5-41dx"
 
-ffmpeg -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -loop 1 -i [IMAGE_FILE] -i [AUDIO_FILE/PLAYLIST_URL] -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p -s 1280x720 -f flv rtmp://[YOUTUBE_RTMP_SERVER_URL]/[YOUTUBE_STREAM_KEY]
+ffmpeg -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -loop 1 -i "$IMAGE_FILE" -i "$AUDIO_FILE_OR_PLAYLIST_URL" -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p -s 1280x720 -f flv "$YOUTUBE_RTMP_SERVER_URL"/"$YOUTUBE_STREAM_KEY"
