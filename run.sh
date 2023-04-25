e54b595783d64abdd6bd6b3e3170d1af25636484
Mytheiw <128908320+Mytheiw@users.noreply.github.com>
2023-04-25 04:22:31 -1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ IMAGE="50"
 AUDIO="44100"
 QUEUE="4096"
 
-ffmpeg -y -re -loop "$LOOP" -i "$IMAGE_FILE" -i "$AUDIO_FILE_OR_PLAYLIST_URL" -c:a libmp3lame -ar "$AUDIO" -b:a 128k  -thread_queue_size "$QUEUE" -c:v libx264 -b:v 1500k -preset veryfast -pix_fmt yuv420p -s 1280x720  -thread_queue_size "$QUEUE" -vb 400k -maxrate 6000k -bufsize 1500k -g "$IMAGE" -f flv -flvflags no_duration_filesize "$YOUTUBE_RTMP_SERVER_URL"/"$YOUTUBE_STREAM_KEY"
+ffmpeg -y -re -loop "$LOOP" -i "$IMAGE_FILE" -i "$AUDIO_FILE_OR_PLAYLIST_URL" -c:a libmp3lame -ar "$AUDIO" -b:a 128k -c:v libx264 -b:v 1500k -preset veryfast -pix_fmt yuv420p -s 1280x720 -vb 400k -maxrate 6000k -bufsize 1500k -g "$IMAGE" -f flv -flvflags no_duration_filesize "$YOUTUBE_RTMP_SERVER_URL"/"$YOUTUBE_STREAM_KEY"
