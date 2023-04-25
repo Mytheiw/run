a0b431441d88c5e2b84ad520e22923b26fe373b0
Mytheiw <128908320+Mytheiw@users.noreply.github.com>
2023-04-25 02:42:17 -1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ AUDIO_FILE_OR_PLAYLIST_URL="http://carbon.bxhosting.com:1780/stream"
 YOUTUBE_RTMP_SERVER_URL="rtmp://x.rtmp.youtube.com/live2"
 YOUTUBE_STREAM_KEY="4mkp-0570-xf06-7gb5-41dx"
 
-ffmpeg -re -loop 1 -i "$IMAGE_FILE" -i "$AUDIO_FILE_OR_PLAYLIST_URL" -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p -s 1280x720 -b:a 712000 -bufsize 512k -f flv "$YOUTUBE_RTMP_SERVER_URL"/"$YOUTUBE_STREAM_KEY"
+ffmpeg -re -loop 1 -i "$IMAGE_FILE" -i "$AUDIO_FILE_OR_PLAYLIST_URL" -c:a copy -shortest -c:v libx264 -b:v 1500k -pix_fmt yuv420p -s 1280x720 -b:a 712000k -bufsize 750k -f flv "$YOUTUBE_RTMP_SERVER_URL"/"$YOUTUBE_STREAM_KEY"
