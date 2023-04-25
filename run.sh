a366666033e36f1ed45af620b870f8e7107e5d83
Mytheiw <128908320+Mytheiw@users.noreply.github.com>
2023-04-25 04:10:26 -1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ YOUTUBE_STREAM_KEY="wkbg-ks7a-dh74-kasj-320w"
 LOOP="1"
 IMAGE="2"
 AUDIO="44100"
+QUEUE="4096"
 
-ffmpeg -re -loop "$LOOP" -i "$IMAGE_FILE" -i "$AUDIO_FILE_OR_PLAYLIST_URL" -c:a libmp3lame -ar "$AUDIO" -b:a 128k -c:v libx264 -b:v 1500k -preset veryfast -pix_fmt yuv420p -s 1280x720 -bufsize 6000k -vb 400k -maxrate 1500k -g "$IMAGE" -f flv -flvflags no_duration_filesize "$YOUTUBE_RTMP_SERVER_URL"/"$YOUTUBE_STREAM_KEY"
+ffmpeg -re -loop "$LOOP" -i "$IMAGE_FILE" -i "$AUDIO_FILE_OR_PLAYLIST_URL" -c:a libmp3lame -ar "$AUDIO" -b:a 128k  -thread_queue_size "$QUEUE" -c:v libx264 -b:v 1500k -preset veryfast -pix_fmt yuv420p -s 1280x720  -thread_queue_size "$QUEUE" -vb 400k -maxrate 6000k -bufsize 1500k -g "$IMAGE" -f flv -flvflags no_duration_filesize "$YOUTUBE_RTMP_SERVER_URL"/"$YOUTUBE_STREAM_KEY"
